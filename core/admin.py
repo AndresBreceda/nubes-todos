3858caf8203b59dfc78259105e1db584027fdbfc
Andrés Esquivel Breceda <a.e.breceda@gmail.com>
2026-07-13 18:35:56 -0600

    Fix: New tab added (Ariel y Cesar)

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Nota
-
-
-@admin.register(Nota)
-class NotaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'texto', 'creado_en')
-    search_fields = ('texto',)
